4e3dcf424a0a69d009d7000f1b51441fcbc49d02
一个疯子 <16155966+a-wild-card@user.noreply.gitee.com>
2026-05-22 23:00:06 +0800

    First

---
+++ Typescript/Fraction.ts
@@ -0,0 +1,132 @@
+/**
+ * 分数系统
+ */
+export class Fraction
+{
+    /**
+     * @param numerator 分子
+     * @param denominator 分母
+     * @param isReduce 是否约分，默认为true
+     */
+    constructor(public numerator: number, public denominator: number ,isReduce: boolean = true)
+    {
+        if(denominator == 0) 
+            throw new Error('Denominator cannot be zero');
+        if(isReduce)
+            this.reduce()
+    }
+
+    private gcd(a: number, b: number): number
+    {
+        //console.log(`Calculating gcd of ${a} and ${b}`)
+        if(b == 0)
+        {
+            return a
+        }
+        else
+            return this.gcd(b,a%b)
+    }
+
+    /**
+     * 约分
+     */
+    reduce()
+    {
+        const gcd = this.gcd(Math.abs(this.numerator),Math.abs(this.denominator))
+        this.numerator /= gcd
+        this.denominator /= gcd
+    }
+
+    /**
+     * 加法
+     * @param {Fraction} other - 另一个分数
+     * @returns @type {Fraction}
+     */
+    add(other: Fraction): Fraction
+    {
+        const newNumerator = this.numerator * other.denominator + other.numerator * this.denominator
+        const newDenominator = this.denominator * other.denominator
+        return new Fraction(newNumerator, newDenominator)
+    }
+
+    /**
+     * 减法
+     * @param {Fraction} other - 另一个分数
+     * @returns @type {Fraction}
+     */
+    sub(other: Fraction): Fraction
+    {
+        const newNumerator = this.numerator * other.denominator - other.numerator * this.denominator
+        const newDenominator = this.denominator * other.denominator
+        return new Fraction(newNumerator, newDenominator)
+    }
+
+    /**
+     * 乘法
+     * @param {Fraction} other - 另一个分数
+     * @returns @type {Fraction}
+     */
+    div(other: Fraction): Fraction
+    {
+        const newNumerator = this.numerator * other.denominator
+        const newDenominator = this.denominator * other.numerator
+        return new Fraction(newNumerator, newDenominator)
+    }
+
+    /**
+     * 除法
+     * @param {Fraction} other - 另一个分数
+     * @returns @type {Fraction}
+     */
+    mul(other: Fraction): Fraction
+    {
+        const newNumerator = this.numerator * other.numerator
+        const newDenominator = this.denominator * other.denominator
+        return new Fraction(newNumerator, newDenominator)
+    }
+
+    /**
+     * 幂
+     * @param {number} exponent - 指数
+     * @returns @type {Fraction}
+     */
+
+    square(exponent:number): Fraction
+    {
+        if(exponent%1 !== 0)
+            throw new Error("Exponent must be an integer.")
+        return new Fraction(this.numerator**exponent, this.denominator**exponent)
+    }
+
+    /**
+     * 求根
+     * @param {number} root - 根的次数
+     * @returns @type {Fraction}
+     */
+    root(root:number): Fraction
+    {
+        if(root%1 !== 0)
+        {
+            throw new Error("Exponent must be an integer.")
+        }
+        return new Fraction(Math.floor(this.toNumber()**(1/root)*10000),10000)
+    }
+
+
+    toNumber(): number
+    {
+        return this.numerator / this.denominator
+    }
+
+    toString():string
+    {
+        return `${this.numerator}/${this.denominator}`
+    }
+}
+
+// console.log(new Fraction(1, 2).add(new Fraction(1, 3)).toString()) // "5/6"
+// console.log(new Fraction(1, 2).sub(new Fraction(1, 3)).toString()) // "1/6"
+// console.log(new Fraction(1, 2).mul(new Fraction(1, 3)).toString()) // "1/6"
+// console.log(new Fraction(1, 2).div(new Fraction(1, 3)).toString()) // "3/2"
+// console.log(new Fraction(1, 2).square(2).toString()) // "1/4"
+// console.log(new Fraction(2,1).root(2).toString()) // "1/2"
